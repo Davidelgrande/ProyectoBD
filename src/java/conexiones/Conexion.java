441ba3421627dccfedd7b17eb48061cb86d08cee
akcel daniel <71154828+AXELICIOSO@users.noreply.github.com>
2021-02-21 15:40:08 -0500

    conexion

--- src/java/conexiones/Conexion.java
+++ src/java/conexiones/Conexion.java
@@ -1,34 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package conexiones;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 
 public class Conexion {
 
-Connection con ;
+    public static Conexion instance;
+    private Connection cnn;
 
-public Connection getConnection(){
-    String url="jdbc:mysql://localhost:3325/comacontento";
-    String user="root";
-    String pass=" ";
-    try {
-        Class.forName("com.mysql.jdbc.Driver");
-        con=DriverManager.getConnection(url,user,pass);
-        
-    }catch (Exception e){
-        
-    }return con ;
-    
-}
+    private Conexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            try {
+                cnn = DriverManager.getConnection("jdbc:mysql://localhost:3325/bd_pedidos", "root", "");
+            } catch (SQLException ex) {
+                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
+    public static synchronized Conexion getInstance() {
+        if (instance == null) {
+            instance = new Conexion();
+        }
+        return instance;
+    }
 
+    public Connection getCnn() {
+        return cnn;
+    }
+
+    public void CerrarConexion() {
+        instance = null;
+    }
 
 
     
